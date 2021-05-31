4adb6556a7918ef33446ef72416f1bdc2c7a24f0
徐东顺 <1533889695@qq.com>
2021-05-31 11:26:45 +0800

    [Initial]初始化

---
+++ Block/Block/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Block
+//
+//  Created by 徐东顺 on 2021/5/31.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
